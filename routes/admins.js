49ce43017fe247d5a706b3066a58b29e4de94e01
Mudassir Ijaz <ijazmudassir786@gmail.com>
2020-06-12 12:16:41 +0500

    admin tested and validations applied

--- routes/admins.js
+++ routes/admins.js
@@ -4,8 +4,9 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 const _ = require("lodash");
 const express = require("express");
-const { Admin } = require("../models/admin");
+const { Admin, validate } = require("../models/admin");
 const router = express.Router();
+const Joi = require("joi");
 
 // getting current admin
 router.get("/me/:id", auth, async (req, res) => {
@@ -19,6 +20,9 @@ router.get("/me/:id", auth, async (req, res) => {
 
 // login
 router.post("/login", async (req, res) => {
+  const { error } = validateLogin(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
   let admin = await Admin.findOne({ email: req.body.email });
   if (!admin)
     return res.status(400).json({ error: "Invalid email or password." });
@@ -33,6 +37,9 @@ router.post("/login", async (req, res) => {
 
 // register
 router.post("/register", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
   let admin = await Admin.findOne({ email: req.body.email });
   if (admin)
     return res.status(400).json({
@@ -43,12 +50,33 @@ router.post("/register", async (req, res) => {
 
   const salt = await bcrypt.genSalt(10);
   admin.password = await bcrypt.hash(admin.password, salt);
-
   await admin.save();
-
   const token = admin.generateAuthToken();
 
   res.header("x-auth-token", token).json({ token });
 });
+
+// function to validate login params
+validateLogin = (req) => {
+  const passwordReg = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+  const schema = {
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string()
+      .regex(RegExp(passwordReg))
+      .required()
+      .options({
+        language: {
+          string: {
+            regex: {
+              base:
+                "must contains 8 digits, one lower case, one upper case and one special character",
+            },
+          },
+        },
+      }),
+  };
+
+  return Joi.validate(req, schema);
+};
 
 module.exports = router;
