a22c9986bed0c8726c3aa7aed8937c9748feced9
Mudassir Ijaz <ijazmudassir786@gmail.com>
2020-06-12 14:33:52 +0500

    job validations applied, but errors in end points

--- models/job.js
+++ models/job.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Joi = require("joi");
 
 const jobSchema = new mongoose.Schema({
   title: { type: String },
@@ -13,4 +14,20 @@ const jobSchema = new mongoose.Schema({
 
 const Job = mongoose.model("Job", jobSchema);
 
+validateJob = (company) => {
+  const schema = {
+    title: Joi.string().min(5).max(50).required(),
+    city: Joi.string().min(5).max(50).required(),
+    area: Joi.string().min(5).max(50).required(),
+    description: Joi.string().min(50).max(100).required(),
+    noOfPositions: Joi.number().required(),
+    yearsOfExperience: Joi.number().required(),
+    salaryRange: Joi.number().required(),
+    company_id: Joi.objectId(),
+  };
+
+  return Joi.validate(company, schema);
+};
+
 exports.Job = Job;
+exports.validate = validateJob;
--- routes/jobs.js
+++ routes/jobs.js
@@ -1,22 +1,24 @@
 const auth = require("../middleware/auth");
 const _ = require("lodash");
-const { Job } = require("../models/job");
+const { Job, validate } = require("../models/job");
 const { JobsApplied } = require("../models/jobs_applied");
 const express = require("express");
-
 const router = express.Router();
 
+// getting a job
 router.get("/:id", async (req, res) => {
   const job = await Job.findById(req.params.id);
-  res.send(job);
+  res.json({ data: job });
 });
 
+// getting all jobs
 router.get("/", auth, async (req, res) => {
   const job = await Job.find();
-  res.send(job);
+  res.json({ data: job });
 });
 
 // :FIXME: // job is not populating only array of object having job_id and employee_id is showing
+// appied jobs for a customer
 router.get("/appliedJobs/:id", async (req, res) => {
   const jobs = await JobsApplied.find({ applied_by: req.params.id })
     .populate("jobs_id")
@@ -25,6 +27,7 @@ router.get("/appliedJobs/:id", async (req, res) => {
 });
 
 // :TODO: searching on title, area etc
+// searching a job
 router.get("/searchjob/:id", async (req, res) => {
   const jobs = await Job.find();
   const query = req.params.id.toLowerCase();
@@ -40,8 +43,11 @@ router.get("/searchjob/:id", async (req, res) => {
   res.send(foundedJobs);
 });
 
+// post a new job
 router.post("/postNewJob", auth, async (req, res) => {
-  const company_id = req.body.company_id;
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
   const job = new Job(
     _.pick(req.body, [
       "title",
@@ -59,7 +65,12 @@ router.post("/postNewJob", auth, async (req, res) => {
   res.json({ message: "Job has been posted successfully", data: job });
 });
 
+// FIXME: updation don't work
 router.put("/:id", auth, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // TODO: if id not found then say you cannot update this job else update it
   const job = req.body;
 
   const jobs = await Job.findByIdAndUpdate(
@@ -67,11 +78,18 @@ router.put("/:id", auth, async (req, res) => {
     { $set: { job } },
     { new: true }
   );
-  res.json({ message: "Job has been updateed successfully" });
+
+  res.json({ message: "Job has been updateed successfully", data: jobs });
 });
+
+// deletion of job
 router.delete("/:id", auth, async (req, res) => {
   const job = await Job.findByIdAndRemove(req.params.id);
-  res.json({ message: "Job has been deleted successfully" });
+  if (!job) {
+    return res.status(400).json({ error: "Job not found" });
+  } else {
+    res.json({ message: "Job has been deleted successfully" });
+  }
 });
 
 module.exports = router;
