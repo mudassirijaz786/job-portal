a0822277f064f87d2784a2e04291d57a96226535
NaeemHHassan <naeemhassancomsats@gmail.com>
2020-06-14 01:20:39 +0500

    jobsFIXED

--- routes/jobs.js
+++ routes/jobs.js
@@ -2,7 +2,10 @@ const auth = require("../middleware/auth");
 const _ = require("lodash");
 const { Job, validate } = require("../models/job");
 const { JobsApplied } = require("../models/jobs_applied");
+const { Employee } = require("../models/employee");
+const { Profile } = require("../models/profile");
 const express = require("express");
+const { log } = require("winston");
 const router = express.Router();
 
 // getting a job by id
@@ -55,16 +58,57 @@ router.get("/", async (req, res) => {
   const job = await Job.find();
   res.json({ data: job });
 });
-
+router.put("/applyForJob", async (req, res) => {
+  console.log(req.body);
+  const job = await Job.findByIdAndUpdate(
+    req.body.job_id,
+    {
+      $push: {
+        applied_by: { employee_id: req.body.applied_by },
+      },
+    },
+    { new: true }
+  );
+  console.log(job);
+  res.json({
+    message: "You've applied the job to the organization successfully",
+  });
+});
 // :FIXME: // job is not populating only array of object having job_id and employee_id is showing
 // appied jobs for a customer
 router.get("/appliedJobs/:id", async (req, res) => {
-  const jobs = await JobsApplied.find({ applied_by: req.params.id })
-    .populate("Job")
-    .exec();
-  res.send(jobs);
+  // params k ander employee id jai gi . mtlb employee nay jo jo b job pay apply kiya howa jo jobs a jy gi
+  const job = await Job.find({
+    applied_by: { $elemMatch: { employee_id: req.params.id } },
+  }).select("-applied_by");
+  // console.log(job);
+
+  // const job = await Job.find(req.params.id)
+  //   .select("applied_by")
+  //   .populate("applied_by")
+  //   .exec();
+  // job.applied_by.forEach((j) => {
+  //   j["password"] = "";
+  // });
+
+  res.send(job);
 });
 
+router.get("/collectCV/:jobId", auth, async (req, res) => {
+  // console.log(req.params.jobId);
+  const jobId = req.params.jobId;
+  const job = await Job.findById(jobId);
+  const profiles = [];
+  for (let i = 0; i < job.applied_by.length; i++) {
+    const element = job.applied_by[i];
+    const pr = await Profile.findOne({ employee_id: element.employee_id })
+      .populate("employee_id")
+      .exec();
+    pr.employee_id["password"] = "";
+    profiles.push(pr);
+  }
+  res.send(profiles);
+});
 // searching a job
 router.get("/searchjob/:id", async (req, res) => {
   const jobs = await Job.find();
